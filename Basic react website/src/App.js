97c974fd8277363d8896b713e7c01fb1fbec13de
mernstackdeveloper <49354823+mernstackdeveloper@users.noreply.github.com>
2020-06-10 16:24:03 +0530

    basic react website

---
+++ Basic react website/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import NavBar from "./Navbar.js";
+import TopSection from "./Section.js";
+import Buttom from "./Buttom";
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <TopSection />
+      <Buttom />
+    </div>
+  );
+};
+
+export default App;
